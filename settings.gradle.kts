a42e9f1bc808ea789f236ecc06f384745d935954
Nico <jumpingpxl@gmail.com>
2025-02-11 13:18:14 +0100

    update labygradle?

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "togglesneak"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.5"
+    val labyGradlePluginVersion = "0.5.8"
 
     buildscript {
         repositories {
