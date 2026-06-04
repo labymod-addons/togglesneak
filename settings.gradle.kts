bc9bd31555875f436b4319926d0b0f9a21840e80
Christian <r0bby@gmx.net>
2026-06-04 05:46:52 +0200

    Update LabyGradle to 0.8.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "togglesneak"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.8.0"
+    val labyGradlePluginVersion = "0.8.1"
 
     buildscript {
         repositories {
